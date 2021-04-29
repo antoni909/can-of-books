330abf92f421d74df51627acae9d1094344b54ca
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-04-28 19:23:02 -0700

    Starting on card 2

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(cors());
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// requiring the model AFTER running mongoose.connect -- connect to the DB before dealing with the things
+const User = require('./models/User');
+
+app.get('/', (request, response) => {
+  response.send('hello from the test, root/home');
+});
+
+// seed the database with some items so we can retrieve them
+// create routes and callbacks
+
+const firstUser = new User({
+  
+})
 
 app.listen(PORT, ()=> console.log(`server listens on PORT:${PORT}`));
